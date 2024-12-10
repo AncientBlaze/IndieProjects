f305cc7ebca7ce6261376d182e4b4435d1dc15e7
AncientBlaze <saikata62@gmail.com>
2024-12-10 23:26:24 +0530

    updated password validation

--- form/script.js
+++ form/script.js
@@ -1,9 +1,25 @@
-document.querySelector('form').addEventListener('submit', function(event) {
+let username;
+let email;
+let password;
+let phone;
+document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
-    const username = document.querySelector('.username').value;
-    const email = document.querySelector('.email').value;
-    const password = document.querySelector('.psw').value;
-    const phone = document.querySelector('.phone').value;
+    username = document.querySelector('.username').value;
+    email = document.querySelector('.email').value;
+    password = document.querySelector('.psw').value;
+    phone = document.querySelector('.phone').value;
 
     alert('Username: ' + username + '\nEmail: ' + email + '\nPassword: ' + password + '\nPhone: ' + phone);
+    console.log('Username: ' + username + '\nEmail: ' + email + '\nPassword: ' + password + '\nPhone: ' + phone);
 });
+
+function togglePassword() {
+    let input = document.querySelector(".password-input input");
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
+
+console.log(username, email, password, phone);
